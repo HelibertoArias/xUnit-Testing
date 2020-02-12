928dd62d4c60ef1be66fd0f57347b0a1a1dd45c8
helibertoarias <helibertoarias@gmail.com>
2020-02-12 14:29:55 -0500

    Test: Share context beetween differents test classes

---
+++ Game/GameEngine.Tests/TestClass1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    public class TestClass1
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _output;
+
+        public TestClass1(GameStateFixture gameStateFixture,
+                            ITestOutputHelper output)
+        {
+            _gameStateFixture = gameStateFixture;
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            _output.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+    }
+
+    [Collection("GameState collection")]
+    public class TestClass2
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _output;
+
+        public TestClass2(GameStateFixture gameStateFixture,
+                            ITestOutputHelper output)
+        {
+            _gameStateFixture = gameStateFixture;
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            _output.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+        }
+    }
+}
